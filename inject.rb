d261f69cb359ed99975847458c171781f4930848
Peter Saxton <peterhsaxton@gmail.com>
2014-01-31 10:47:04 +0000

    Two versions of recursive inject added

--- inject.rb
+++ inject.rb
@@ -5,4 +5,15 @@ class Array
 		end
 		init
 	end
+
+	def recursive_inject init=0, &block
+		element = shift
+		return init if element.nil?
+		init = yield(init, element)
+		recursive_inject(init, &block)
+	end
+
+	def recursive_inject init=0, &block
+		(element = shift) ? recursive_inject((yield init, element), &block) : init 
+	end
 end
