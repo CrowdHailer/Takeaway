4d3b4163bbce3ddfe1ef02081b4b1cb0b9a69ee8
Peter Saxton <peterhsaxton@gmail.com>
2014-01-31 12:09:00 +0000

    Check for price equalling payment

--- spec/takeaeway_spec.rb
+++ spec/takeaeway_spec.rb
@@ -1,35 +1,51 @@
 require './takeaway'
 
 describe Takeaway do
-	let(:chicken_shop) { Takeaway.new }
+	let(:takeaway) { Takeaway.new }
 	let(:fried_chicken) { {name: "fried chicken", price: 1.24} }
-	let(:chicken_burger) { {name: "chicken_burger", price: 2.30}}
+	let(:chicken_burger) { {name: "chicken burger", price: 2.30} }
 
 	it 'should be initialized with no dishes' do
-		expect(chicken_shop.dishes).to be_empty
+		expect(takeaway.dishes).to be_empty
 	end
 
 	it 'should be possible to add dishes' do
-		chicken_shop.add_dish fried_chicken
-		expect(chicken_shop.dishes).to eq [fried_chicken]
+		takeaway.add_dish fried_chicken
+		expect(takeaway.dishes).to eq [fried_chicken]
 	end
 
 	it 'should be able to list dishes and their price' do
-		chicken_shop.add_dish fried_chicken
-		expect(chicken_shop.list_dishes).to eq "1. fried chicken;     £1.24"
+		takeaway.add_dish fried_chicken
+		expect(takeaway.list_dishes).to eq "1. fried chicken;     £1.24"
 	end
 
 	it 'should be able to list multiple dishes' do
-		chicken_shop.add_dish fried_chicken
-		chicken_shop.add_dish chicken_burger
-		expect(chicken_shop.list_dishes).to eq(
+		takeaway.add_dish fried_chicken
+		takeaway.add_dish chicken_burger
+		expect(takeaway.list_dishes).to eq(
 			"1. fried chicken;     £1.24\n2. chicken burger;    £2.30"
 		)
 	end
 
 	it 'should return the time 1hr from now' do
-		arrival = chicken_shop.arrival_time
+		arrival = takeaway.arrival_time
 		puts arrival
 		expect(arrival).to match /\d{2}:\d{2}/
+	end
+
+	context 'placeing an order' do
+		let(:order) { {dishes: ["fried chicken", "chicken burger"], quantities: [2,1], payment: 4.78} }
+		let(:wrong_order) { {dishes: ["fried chicken"], quantities: [1], payment: 24 } }
+		let(:chicken_shop) do
+			takeaway = Takeaway.new
+			takeaway.add_dish fried_chicken
+			takeaway.add_dish chicken_burger
+			takeaway
+		end
+
+		it 'should check the order total' do
+			expect{chicken_shop.place_order order}.not_to raise_error
+			expect{chicken_shop.place_order wrong_order}.to raise_error "Incorrect total"
+		end
 	end
 end
--- takeaway.rb
+++ takeaway.rb
@@ -11,9 +11,25 @@ class Takeaway
 
 	def list_dishes
 		strings = dishes.map.with_index do |item, i|
-			"#{i+1}. #{(item[:name].gsub('_', ' ')+';').ljust(19)}£#{'%.2f' % item[:price]}"
+			"#{i+1}. #{(item[:name]+';').ljust(19)}£#{'%.2f' % item[:price]}"
 		end
 		strings.join "\n"
+	end
+
+	def place_order order
+		correct_payment order
+
+	end
+
+	def correct_payment order
+		new_orders = order[:dishes].zip(order[:quantities])
+		new_orders.map! do |item|
+			name, quantity = item
+			dish = dishes.find{|dish| dish[:name] == name}
+			dish[:price] * quantity
+		end
+		price = new_orders.inject(:+).round(2)
+		raise "Incorrect total" unless price == order[:payment]
 	end
 
 	def arrival_time
