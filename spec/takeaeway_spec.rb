686a1ecb84dfbab6bf4b14c0e7dffb332f1410a9
Peter Saxton <peterhsaxton@gmail.com>
2014-01-31 11:17:53 +0000

    Displays formatted menu and pads with zeros correctly.

--- spec/takeaeway_spec.rb
+++ spec/takeaeway_spec.rb
@@ -2,12 +2,28 @@ require './takeaway'
 
 describe Takeaway do
 	let(:chicken_shop) { Takeaway.new }
+	let(:fried_chicken) { {name: "fried chicken", price: 1.24} }
+	let(:chicken_burger) { {name: "chicken_burger", price: 2.30}}
+
 	it 'should be initialized with no dishes' do
 		expect(chicken_shop.dishes).to be_empty
 	end
 
 	it 'should be possible to add dishes' do
-		chicken_shop.add_dish({name: "fried chicken", price: "£1.24"})
-		expect(chicken_shop.dishes).to eq([{name: "fried chicken", price: "£1.24"}])
+		chicken_shop.add_dish fried_chicken
+		expect(chicken_shop.dishes).to eq [fried_chicken]
+	end
+
+	it 'should be able to list dishes and their price' do
+		chicken_shop.add_dish fried_chicken
+		expect(chicken_shop.list_dishes).to eq "1. fried chicken;     £1.24"
+	end
+
+	it 'should be able to list multiple dishes' do
+		chicken_shop.add_dish fried_chicken
+		chicken_shop.add_dish chicken_burger
+		expect(chicken_shop.list_dishes).to eq(
+			"1. fried chicken;     £1.24\n2. chicken burger;    £2.30"
+		)
 	end
 end
--- takeaway.rb
+++ takeaway.rb
@@ -8,4 +8,11 @@ class Takeaway
 	def add_dish item
 		dishes << item
 	end
+
+	def list_dishes
+		strings = dishes.map.with_index do |item, i|
+			"#{i+1}. #{(item[:name].gsub('_', ' ')+';').ljust(19)}£#{'%.2f' % item[:price]}"
+		end
+		strings.join "\n"
+	end
 end
